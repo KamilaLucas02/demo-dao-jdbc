7d8616b94dd3131fd1df6ddfa8dfe4b0cd5a586f
Kamila <kamila2lucas@gmail.com>
2022-10-19 22:44:25 -0300

    finAll implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,11 +16,17 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("=== TESTE 1:  seller findById ===");
+		System.out.println("=== TESTE 2:  seller findByDepartment ===");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
+		}
+		
+		System.out.println("=== TESTE 2:  seller findByIdAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+		System.out.println(obj);
 		}
 	}
 
